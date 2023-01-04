2759cf48ee5f36664591ac15a6c8b0f16b067ff3
fufulouteur <manzodqb@icloud.com>
2023-01-04 17:21:46 -0500

    fini le pojet python_un _peu_horticulture + modife

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-pot = int(input('combien de pots de fleurs ?: '))
-graines = int(input('combien de sachets de graines ?: '))
-terreau = int(input('combien de sacs de terreau ?: '))
+
+print("graines, terreau, pot, caisse ou exit : ")
+pot = input()
+if pot == 'pot':
+    pot = int(input('combien de pots de fleurs ?: '))
+
+
+print("graines, terreau, pot, caisse ou exit : ")
+graines = input()
+if graines == 'graines':
+    graines = int(input('combien de graines ?: '))
+
+print("graines, terreau, pot, caisse ou exit : ")
+terreau = input()
+if terreau == 'terreau':
+    terreau = int(input('combien de terreau ?: '))
 
 prix_pot = 4.00
 prix_graines = 1.00
 prix_terreau = 5.00
 taux_TVA = 0.20
 
-total_HT = (pot * prix_pot) + (graines * prix_graines) + (terreau * prix_terreau)
+if print('caisse'):
+    total_HT = 'pot' + 'terreau' + 'graines'
+    print("total hors taxe : ")
+    print(total_HT)
+
+    total_TTC = total_HT + (total_HT * taux_TVA)
+    print("montant a payer : ")
+    print(total_TTC)
+
+else:
+    total_HT = (pot * prix_pot) + (graines * prix_graines) + (terreau * prix_terreau)
 print("total hors taxe : ")
 print(total_HT)
 
